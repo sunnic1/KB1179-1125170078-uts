1fbe67c5b8a9cea28e5c5aaef43204f9f38c3d64
Rifqi Arip Sunni M <ariefsunni@gmail.com>
2025-10-05 11:01:05 +0700

    initial new project dart

---
+++ bin/season_3_practice.dart
@@ -0,0 +1,5 @@
+import 'package:season_3_practice/season_3_practice.dart' as season_3_practice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${season_3_practice.calculate()}!');
+}
---
+++ test/season_3_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:season_3_practice/season_3_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
